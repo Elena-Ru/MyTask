4af63ed20fefde078ca302cc0a6651ad47e14270
Elena <lenarusskih@gmail.com>
2023-05-10 12:24:28 +0300

    Initial commit

---
+++ MyTask/MyTask/TaskViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyTask
+//
+//  Created by Елена Русских on 10.05.2023.
+//
+
+import UIKit
+
+class TaskViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .systemPink
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
